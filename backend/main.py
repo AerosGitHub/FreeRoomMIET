7f79e7b3dc1da9ac98d90c3e75f2d1f0b700b5f2
Aeros <aeros.mainmail@gmail.com>
2026-02-03 14:21:02 +0300

    new code

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
 import sqlite3
 
-from sqlalchemy import create_engine, MetaData, inspect
+from sqlalchemy import create_engine, MetaData, inspect, select, func
 from sqlalchemy.orm import sessionmaker
-
-from models import Base
+from models import Base, Group, AllBase
 
 engine = create_engine('sqlite:///../db/database.db')
 inspetor = inspect(engine)
 Session = sessionmaker(bind=engine)
+
+with Session() as session:
+    try:
+        for i in session.scalars(select(AllBase.group, func.count(AllBase.name).label('count')).group_by(AllBase.group)).all():
+            print(i.group, i.count)
+    except:
+        session.rollback()
+        raise
+    else:
+        session.commit()
 
